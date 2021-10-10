6d58ed167d6b67f01047515c5580c25cd8de04fb
Krista Marbury <laetaiustus@gmail.com>
2021-10-10 15:20:57 -0500

    got the about me page looking better, the picture is great. Bootstrap is slowly making sense.

--- src/pages/ParentResources.js
+++ src/pages/ParentResources.js
@@ -4,7 +4,22 @@ import React from "react";
 const ParentResources = () => {
   return (
     <main>
-      <p></p>
+      <div class="row featurrette">
+        <div class="col-md-7">
+          <h2 class="featurrette-heading">Dyslexia</h2>
+          <p class="lead">
+            There are many myths about dyslexia. Susan Barton addresses several
+            of these on her website here:
+            https://www.dys-add.com/dyslexia.html#anchorMyth With those in mind,
+            Let’s get some proper answers…
+          </p>
+          <h2 class="featurrette-heading">What is Dyslexia?</h2>
+          <p class="lead">
+            The short answer is: Dyslexia is a learning disorder that makes it
+            very difficult to read, and write.
+          </p>
+        </div>
+      </div>
     </main>
   );
 };
