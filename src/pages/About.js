9be457d5304c19399657a2a4a246f56632277968
Krista Marbury <laetaiustus@gmail.com>
2021-10-14 16:01:39 -0500

    trying to figure out how to wrap text. Not going to plan yet.

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,43 +5,42 @@ import afro from "./images/momafro.jpeg";
 const About = () => {
   return (
     <main class="container">
-      <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
-        <div class="row">
+      <div class="row">
+        <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
           <div class="col-sm-5 col-md-6">
             <h1 class="display-4 fst-italic">
               Barbara Marbury <br></br>
+              {/* make this text smaller than the name */}
               <h2>
                 <span class="text-muted">
                   M.A.T., Certified Academic Language Therapist (CALT)
                 </span>
               </h2>
+              <img
+                class="d-block mx-auto mb-4 img-responsive afropic"
+                src={afro}
+                alt="headshot of Barbara Marbury"
+              />
             </h1>
-
-            <p class="lead my-3 pull-left">
-              Hello! I am a native Washingtonian and love the city life. My
-              husband and I moved to Chicago in 2017 and quickly fell in love
-              with life in the Windy City. Together, we have three daughters and
-              one received a dyslexia diagnosis – just one year after I’d
-              received my masters degree in teaching. I was a 12th grade English
-              teacher and my 11-year old was struggling with reading and
-              spelling. Determined to help her, I embarked on a journey of
-              training that resulted in my becoming a Certified Academic
-              Language Therapist. How did my daughter fare? She graduated from
-              high school a year early and went on to complete her bachelors
-              degree. She’s now a lover of books and is able to demonstrate her
-              intelligence on paper easily. Her two siblings, inspired by the
-              transformation they saw in their sister, went on to become
-              Academic Language Therapists as well. That diagnosis in 2006
-              changed us as a family. We have gone on to help many other
-              students and would love to help your child and you!
-            </p>
-            <img
-              class="d-block mx-auto mb-4 img-responsive afropic"
-              src={afro}
-              alt="headshot of Barbara Marbury"
-            />
           </div>
-          {/* Make the photo more square and then let the text flow under it */}
+
+          <p class="lead">
+            Hello! I am a native Washingtonian and love the city life. My
+            husband and I moved to Chicago in 2017 and quickly fell in love with
+            life in the Windy City. Together, we have three daughters and one
+            received a dyslexia diagnosis – just one year after I’d received my
+            masters degree in teaching. I was a 12th grade English teacher and
+            my 11-year old was struggling with reading and spelling. Determined
+            to help her, I embarked on a journey of training that resulted in my
+            becoming a Certified Academic Language Therapist. How did my
+            daughter fare? She graduated from high school a year early and went
+            on to complete her bachelors degree. She’s now a lover of books and
+            is able to demonstrate her intelligence on paper easily. Her two
+            siblings, inspired by the transformation they saw in their sister,
+            went on to become Academic Language Therapists as well. That
+            diagnosis in 2006 changed us as a family. We have gone on to help
+            many other students and would love to help your child and you!
+          </p>
         </div>
       </div>
       <div class="row mb-2">
---
+++ src/pages/WhatClientsSay.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import victor from "./images/victorhugo.jpeg";
+
+const whatclientssay = () => {
+  return (
+    <div class="col-md-12">
+      <div class="pull-left">
+        <img src={victor} />
+      </div>
+      <div class="pull-left">Your text goes here......</div>
+    </div>
+  );
+};
+
+export default whatclientssay;
