712275d7dde844cc80eb640f13a01d06732a89a9
Krista Marbury <laetaiustus@gmail.com>
2022-01-18 18:48:13 -0600

    tried to make the homepage a bit more palatable. Not really taking. I'm close, but have not been able to make the image sit nicely next to the quote.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,94 +2,33 @@ import React from "react";
 import openerquote from "./images/momquote1.jpg";
 import victorhugo from "./images/victorhugo.jpg";
 import momafro from "./images/momafro.jpeg";
-import robertbrault from "./images/robertbrault.jpg";
-import nounbrain from "./images/nounbrain.png";
+
 import { Link } from "react-router-dom";
 // import Typewriter from "typewriter-effect";
 
 const Home = () => {
   return (
     <main>
-      <section class="px-4 py-5 my-5 text center">
-        <img
-          class="d-block mx-auto mb-4 img-responsive"
-          src={openerquote}
-          alt="quote about learning to read from barbara marbury"
-        />
-        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          {/* Be sure to make these buttons go to glowing reviews from Clients and ASDEC */}
-          <Link
-            class="btn btn-light btn-lg px-4 gap-3"
-            to="whatclientssay"
-            role="button"
-          >
-            What Clients Say
-          </Link>
-        </div>
-      </section>
-      <section class="px-4 py-5 my-5 text center">
-        <img
-          class="d-block mx-auto mb-4 img-responsive"
-          src={momafro}
-          alt="quote about learning to read from victor hugo"
-        />
-        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          {/* Be sure to make these buttons go to glowing reviews from Clients and ASDEC */}
-          <Link
-            class="btn btn-light btn-lg px-4 gap-3"
-            to="aboutme"
-            role="button"
-          >
-            About Me
-          </Link>
-        </div>
-      </section>
-      <section class="px-4 py-5 my-5 text center">
-        <img
-          class="d-block mx-auto mb-4 img-responsive"
-          src={victorhugo}
-          alt="quote about learning to read from victor hugo"
-        />
-        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          {/* Be sure to make these buttons go to glowing reviews from Clients and ASDEC */}
-          <Link
-            class="btn btn-light btn-lg px-4 gap-3"
-            to="educatorresources"
-            role="button"
-          >
-            Educator Resources
-          </Link>
-        </div>
-      </section>
-      <section class="px-4 py-5 my-5 text center">
-        <img
-          class="d-block mx-auto mb-4 img-responsive"
-          src={robertbrault}
-          alt="quote about looking back by robert brault"
-        />
-        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          {/* Be sure to make these buttons go to glowing reviews from Clients and ASDEC */}
-          <Link
-            class="btn btn-light btn-lg px-4 gap-3"
-            to="parentresources"
-            role="button"
-          >
-            Parent Resources
-          </Link>
-        </div>
-      </section>
-      <section class="px-4 py-5 my-5 text center">
-        <img
-          class="d-block mx-auto mb-4 img-responsive"
-          src={nounbrain}
-          alt="cartoon brain inside of a head"
-        />
-        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          {/* Be sure to make these buttons go to glowing reviews from Clients and ASDEC */}
-          <Link class="btn btn-light btn-lg px-4 gap-3" to="faq" role="button">
-            Frequently Asked Questions
-          </Link>
-        </div>
+      <section class="homepage">
+        <p>
+          <img
+            class="mommafro"
+            src={momafro}
+            alt="picture of Barbara Marbury"
+          />
+        </p>
+        <p class="momq">
+          "Reading is a fundamental civil right, but for students with dyslexia
+          and related learning difficulties, that right is jeopardized. Academic
+          language therapy is transformative. It allows students to experience
+          the satisfaction of reading and writing, meeting their academic
+          potential, and, ultimately, achieving their dreams. As an academic
+          therapist, I work with students to demystify the science of reading
+          and spelling. Along the way, I help them build a robust vocabulary,
+          strengthen their study skills, and develop an appreciation for
+          language that will serve as a foundation for all other learning in
+          life." -Barbara Marbury
+        </p>
       </section>
     </main>
   );
